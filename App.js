27ce2c4fb4ff7a96b7a342ef81ddbb793a334122
Dan Possehl <danielbpossehl@gmail.com>
2021-12-23 14:50:49 -0700

    3 buttons styled

--- App.js
+++ App.js
@@ -1,20 +1,133 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Dan</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5372F0",
+      }}
+    >
+      <View
+        style={{
+          width: "90%",
+          backgroundColor: "#fff",
+          borderRadius: 20,
+          padding: 20,
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 26,
+            fontWeight: "600",
+            color: "#333",
+            marginBottom: 20,
+          }}
+        >
+          Quote of the day
+        </Text>
+        <FontAwesome5
+          name="quote-left"
+          style={{ fontsize: 20, marginBottom: -12 }}
+          color="#000"
+        />
+        <Text
+          style={{
+            color: "#000",
+            fontSize: 16,
+            lineHeight: 26,
+            letterSpaceing: 1.1,
+            fontWeight: "400",
+            textAlign: "center",
+            paddingHorizontal: 30,
+          }}
+        >
+          The Quote of the day is as follows: It is what it is until it is not.
+        </Text>
+        <FontAwesome5
+          name="quote-right"
+          style={{ fontsize: 20, textAlign: "right", marginTop: -20 }}
+          color="#000"
+        />
+        <Text
+          style={{
+            textAlign: "right",
+            fontWeight: "300",
+            fontStyle: "italic",
+            fontSize: 16,
+            color: "#000",
+            marginVertical: 15,
+          }}
+        >
+          -- Author Name
+        </Text>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={{
+            backgroundColor: "#5372f0",
+            padding: 20,
+            borderRadius: 30,
+            marginVerticle: 20,
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 18, textAlign: "center" }}>
+            New Quote
+          </Text>
+
+          {/* 3 BUTTONS*/}
+
+          {/* Speak back button */}
+        </TouchableOpacity>
+        <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{
+              borderWidth: 2,
+              borderColor: "#5372F0",
+              borderRadius: 50,
+              padding: 15,
+              marginTop: 15,
+            }}
+          >
+            {/* Copy button */}
+
+            <FontAwesome name="volume-up" size={22} color="#5372F0" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{
+              borderWidth: 2,
+              borderColor: "#5372F0",
+              borderRadius: 50,
+              padding: 15,
+              marginTop: 15,
+            }}
+          >
+            <FontAwesome5 name="copy" size={22} color="#5372F0" />
+          </TouchableOpacity>
+
+          {/* Twitter button */}
+
+          <TouchableOpacity
+            onPress={() => {}}
+            style={{
+              borderWidth: 2,
+              borderColor: "#5372F0",
+              borderRadius: 50,
+              padding: 15,
+              marginTop: 15,
+            }}
+          >
+            <FontAwesome name="twitter" size={22} color="#5372F0" />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
